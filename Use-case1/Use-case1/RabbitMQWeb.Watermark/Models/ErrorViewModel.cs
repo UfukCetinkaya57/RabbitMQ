4ade31b05dd8d69f01591896d7933ae21a8e9f7b
Ufuk <Ufuk@DESKTOP-8T9IQ2C>
2024-10-07 00:08:24 +0300

    Success Updated

--- Use-case1/Use-case1/RabbitMQWeb.Watermark/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RabbitMQWeb.Watermark.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
